328f0f762ab643594a805c0e14fe648efbd922d8
MarinaPorokhnya <marina.vbyaby.poroxnya@mail.ru>
2024-07-08 19:42:21 +0300

    Добавили решение задач

---
+++ Задача3Л3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Функция, которая обнулит чётные элементы массива. 
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+// При передаче аргумента НИ В КОЕМ случае не указывать тип данных,
+// кампилятору уже известен тип, это приведет к дублированию и синтаксической ошибке
+PrintArray(array);
